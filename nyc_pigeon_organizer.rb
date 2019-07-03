ebd4c20b418e47f59157ec747a80c695490106f2
Austin Trow <austintrow@gmail.com>
2019-07-03 02:31:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,13 +8,9 @@ def nyc_pigeon_organizer(data)
     data_values.each do |value|
       data[key][value].each do |name|
         if !organized.has_key?(name)
-          organized[name] = {
-            color: [],
-            gender: [],
-            lives: []
-          }
-        # else if !organized[name].has_key?(key)
-        #   organized[name][key] = value
+          organized[name] = {}
+        else if !organized[name].has_key?(key)
+           organized[name][key] = value
         else
           organized[name][key] << value.to_s
         end
